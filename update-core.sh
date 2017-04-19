c804f62f3276704aea81385a2533fedff1e2cdab
Christian Nikkanen <hi@kisu.li>
2017-04-19 21:50:21 +0300

    Simplify even more

--- update-core.sh
+++ update-core.sh
@@ -4,9 +4,18 @@ MYPWD=${PWD}
 if [[ $MYPWD != *"config"* ]]; then
   mkdir tmp
   git clone https://github.com/Seravo/wordpress.git tmp/wordpress
+  mkdir config
+  yes | cp -f composer.json config/composer.json
+  yes | cp -f package.json config/package.json
+  yes | cp -f update-core.sh config/update-core.sh
+  yes | cp -f install.js config/install.js
+  yes | cp -f yarn.lock config/yarn.lock
+
   rsync -rv --ignore-times --exclude=*.warning ./config/  tmp/wordpress
   rsync --exclude=.git --exclude=composer.lock --exclude=config-sample.yml --exclude=gulpfile.js -rv tmp/wordpress/ .
   rm -rf tmp
+  rm -rf config
+
   # rm *.warning
   exit
 fi
