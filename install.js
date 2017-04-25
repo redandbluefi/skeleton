60818705ec83137f9f440cecf3145428b9d5eb32
Christian Nikkanen <hi@kisu.li>
2017-04-25 14:42:23 +0300

    Clean installer, remove nodegit

--- install.js
+++ install.js
@@ -7,7 +7,6 @@ const chalk = require('chalk');
 const prompt = require('prompt-sync')({
   sigint: false
 });
-const Git = require('nodegit');
 const program = require('commander');
 const yaml = require('js-yaml');
 
@@ -95,76 +94,17 @@ function createNewProject() {
 }
 
 function setupRepository() {
-  Git.Repository.open(__dirname).then(repo => {
-    const url = 'git@github.com:'.concat(typeof program.repository === 'string'
-      ? program.repository
-      : console.log('Repository path, example: redandbluefi/skeleton') ||
-        prompt('Make it if you don\'t have it yet: ')).concat('.git');
+  const url = 'git@github.com:'.concat(typeof program.repository === 'string'
+    ? program.repository
+    : console.log('Repository path, example: redandbluefi/skeleton') ||
+      prompt('Make it if you don\'t have it yet: ')).concat('.git');
+  const upstream = package.repository.url
+    .replace('git+ssh://', '')
+    .replace('github.com/', 'github.com:')
+    .concat('.git');
 
-
-    // Why is this so hard?
-    // const addUpstream = Git.Remote.setUrl(repo, 'origin', WORDPRESS_URL);
-    // if(addUpstream !== 0) {
-    // console.log(chalk.yellow(addUpstream));
-    // }
-
-    const upstream = package.repository.url
-      .replace('git+ssh://', '')
-      .replace('github.com/', 'github.com:')
-      .concat('.git');
-    cp.execSync(`git remote add upstream ${upstream}`);
-
-    const changeOrigin = Git.Remote.setUrl(repo, 'origin', url);
-    if (changeOrigin !== 0) {
-      // nodegit error
-      console.log(chalk.yellow(changeOrigin));
-    }
-  });
+  cp.execSync(`git remote add upstream ${upstream}`);
+  cp.execSync(`git remote remove origin`);
+  cp.execSync(`git remote add origin ${url}`);
 }
-
-
-// function cloneWordPressBase() {
-  // return new Promise((resolve, reject) => {
-    // Git.Clone(WORDPRESS_URL, TMP_DIR, {
-      // callbacks: {
-        // certificateCheck: () => 1,
-        // credentials: (url, username) => {
-          // return Git.Cred.sshKeyFromAgent(username);
-        // }
-      // }
-    // })
-      // .then(repo => {
-        // fs.rmdir(path.join(TMP_DIR, '.git'));
-        // fs.readdir(TMP_DIR, null, (err, files) => {
-          // if (err) console.error(err);
-
-          // const filecount = files.length;
-          // let i = 0;
-          // files.forEach(file => {
-            // fs.stat(file, (err, stats) => {
-              // if (err) console.error(err);
-              // if (!stats.isFile() && !stats.isDirectory()) {
-                // fs.rename(file, path.join(file, '..'), () => {
-                  // i++;
-                  // console.log(chalk.green(`${file} moved.`));
-                // });
-              // } else {
-                // i++;
-                // console.log(chalk.yellow(`${file} skipped.`));
-              // }
-            // });
-
-            // if (i === filecount - 1) {
-              // resolve('Files moved');
-            // }
-          // });
-        // });
-
-        // fs.rmdir(TMP_DIR);
-      // }).catch(err => {
-        // console.error(err)
-        // reject(err);
-      // });
-  // });
-// }
 
